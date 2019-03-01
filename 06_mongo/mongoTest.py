4eb0e8aca5bd3dc7ebb7b204099bb0bf713cc46d
AntonDanylenko <adanylenko@stuy.edu>
2019-03-01 14:04:01 -0500

    s

---
+++ 06_mongo/mongoTest.py
@@ -0,0 +1,9 @@
+import pymongo
+from pprint import pprint
+
+SERVER_ADDR="104.248.119.62"
+connection = pymongo.MongoClient(SERVER_ADDR)
+db = connection.test
+collection = db.restaurants
+
+[pprint(post) for post in collection.find({"name":"Wendy's"})]
