480c36915ceefd6513dddc884ec832c6e970026a
AntonDanylenko <adanylenko@stuy.edu>
2019-04-17 14:43:37 -0400

    s

--- 20_cw/20_cw.py
+++ 20_cw/20_cw.py
@@ -1,19 +1,34 @@
 from functools import reduce
 
 fd = open('book.txt', 'r')
-f = fd.read()
+book = fd.read().lower()
 fd.close()
 
-#print(f)
 
-print(f.count('proletariat'))
+print("Count of proletariat: " + str(book.count('proletariat')))
 
-
-book = f
-alhpabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 words = []
 current = ""
-for char in f:
-    
+for char in book:
+    if char in alphabet:
+        current+=char
+    elif current!="":
+        words.append(current)
+        current=""
+#print(words)
 
-#print(f)
+# count=0
+# for x in words:
+#     if x=="proletariat":
+#         count+=1
+# print(count)
+
+common_words = ["the", "and", "to", "by", "is", "in", "of", "a", "that", "it", "he", "she", "them"]
+max_word = ""
+max_count = 0
+for x in words:
+    if words.count(x)>max_count and x not in common_words:
+        max_count = words.count(x)
+        max_word = x
+print("Max word: [" + str(max_word) + "], count: " + str(max_count))
