6f9870475b66142f8b40e8bcd12605f15d147513
AntonDanylenko <adanylenko@stuy.edu>
2019-04-18 14:16:09 -0400

    s

--- 20_anon-reduce/20_cw.py
+++ 20_anon-reduce/20_cw.py
@@ -4,11 +4,10 @@ fd = open('book.txt', 'r')
 book = fd.read().lower()
 fd.close()
 
-
-print("Count of proletariat: " + str(book.count('proletariat')))
-print("Count of 'working class': " + str(book.count('working class')))
-
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+# z=0
+# words = [x for x in book if x in alphabet else ',' if ]
+# words.split(',')
 words = []
 current = ""
 for char in book:
@@ -18,6 +17,10 @@ for char in book:
         words.append(current)
         current=""
 #print(words)
+
+print("Count of proletariat: " + str(book.count('proletariat')))
+print("Count of 'working class': " + str(book.count('working class')))
+
 
 # count=0
 # for x in words:
@@ -33,3 +36,11 @@ for x in words:
         max_count = words.count(x)
         max_word = x
 print("Max uncommon word: " + str(max_word) + "; Count: " + str(max_count))
+
+max_char = ""
+max_count_char = 0
+for x in book:
+    if book.count(x)>max_count_char:
+        max_count_char = book.count(x)
+        max_char = x
+print("Max character: " + str(max_char) + "; Count: " + str(max_count_char))
